9ee92adea2b43ae2e3b001ca08d6d6ec693b8baf
Tusharjagi <tusharjagi@gmail.com>
2022-02-24 12:43:16 +0530

    update

--- 1_Array/Searching/Interpolation_Search.cpp
+++ 1_Array/Searching/Interpolation_Search.cpp
@@ -22,10 +22,10 @@ int interpolation_Search(int arr[], int key, int arrSize)
         {
             start = position + 1;
         }
-        return   - 1;
+        
         
     }
-    
+    return   - 1;
 
 
 }
@@ -34,7 +34,7 @@ int main(){
 // 1. sorted array 
 // 2. Uniformly distributed
     int arr[10] = {10,20,30,40,50,60,70,80,90,100}; 
-    int key = 70;
+    int key = 80;
     int arrSize = sizeof(arr)/sizeof(arr[0]);
     int result = interpolation_Search(arr, key, arrSize);
     cout<<"Result of index :: "<<result;
