ea48edf447c49591c9541eeaf8100f057c4a00cf
Tusharjagi <tusharjagi@gmail.com>
2022-02-25 12:56:13 +0530

    update

--- 1_Array/Sort/Insertion_Sort.cpp
+++ 1_Array/Sort/Insertion_Sort.cpp
@@ -1,14 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void printArray (int arr[] , int size)
+void printArray(int arr[], int size)
 {
-    for (int  i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
-        cout<<arr[i] <<" ";
+        cout << arr[i] << " ";
     }
-    cout<<endl;
-    
+    cout << endl;
 }
 
 void Insertion_Sort(int arr[], int size)
@@ -16,23 +15,21 @@ void Insertion_Sort(int arr[], int size)
     for (int i = 1; i < size; i++)
     {
         int current = arr[i];
-        int j = i-1;
-        while (current < arr[j] && j>=0)
+        int j = i - 1;
+        while (current < arr[j] && j >= 0)
         {
-            arr[j+1] = arr[j];
+            arr[j + 1] = arr[j];
             j--;
         }
-        arr[j+1] = current;
-        
+        arr[j + 1] = current;
     }
-    
-    
 }
 
-int main(){
-    int arr[] = {12,45,23,51,19,8};
-    int size = sizeof(arr)/sizeof(arr[0]);
+int main()
+{
+    int arr[] = {12, 45, 23, 51, 19, 8};
+    int size = sizeof(arr) / sizeof(arr[0]);
     Insertion_Sort(arr, size);
-    printArray(arr,size);
+    printArray(arr, size);
     return 0;
 }
