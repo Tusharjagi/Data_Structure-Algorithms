5e0cf8e257dd7ad9f841e8732aff2c07f6da6fdc
Tusharjagi <tusharjagi@gmail.com>
2022-03-03 23:57:19 +0530

    Linked_list

--- 2_Linked_list/doubly_Linked_list.cpp
+++ 2_Linked_list/doubly_Linked_list.cpp
@@ -1,7 +1,78 @@
 #include<iostream>
 using namespace std;
 
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAthead(node* &head, int val )
+{
+    node* n=new node(val);
+    n->next=head;
+    head=n;
+}
+
+void InsertAtTail(node* &head, int val){
+    node* n = new node (val);
+
+    if(head==NULL){
+        head=n;
+        return;
+    }
+    node* temp=head;
+    while (temp->next!=NULL)
+    {
+       temp=temp->next;
+    }
+    temp->next=n;
+    
+}
+
+void Display(node* head){
+    node* temp=head;
+    while (temp!=NULL)
+    {
+       cout<<temp->data<<"->";
+       temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+    
+}
+
+bool search(node* head, int key)
+{
+    node* temp = head;
+    while (temp!=NULL)
+    {
+        if (temp->data==key)
+        {
+            return true;
+        }
+        temp=temp->next;
+        
+    }
+    return false;
+    
+}
+
 int main(){
+    node* head=NULL;
+    InsertAtTail(head,1);
+    InsertAtTail(head,2);
+    InsertAtTail(head,3);
+    InsertAtTail(head,4);
+    InsertAtTail(head,5);
+    insertAthead(head,7);
+    Display(head);
+    cout<<search(head,4)<<endl;
+
     
     return 0;
 }
