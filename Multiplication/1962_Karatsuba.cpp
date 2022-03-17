7c9df28bf0ddc73fc5218a6710fc8c7619688be1
Tusharjagi <tusharjagi@gmail.com>
2022-03-18 00:53:14 +0530

    Karatsuba Multiplication

---
+++ Multiplication/1962_Karatsuba.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+/*
+
+---------> Karatsuba Algorithm <-------
+-> Using Divide and Conquer
+-> Always perform EVEN numbers
+
+
+Step 0 : Divide the number two equal half a & b , a & c
+Step 1 : Compute a.c              -> S1
+Step 2 : Compute b.d              -> S2
+Step 3 : Compute (a+b) * (c+d)    -> S3
+Step 4 : S3-S2-S1                 -> S4
+Step 5 : S1 * (b^n) + S4 * (b^n/2) + S2
+
+b = Base
+n = Number of digit
+
+*/
+
+int main(){
+    
+    return 0;
+}
